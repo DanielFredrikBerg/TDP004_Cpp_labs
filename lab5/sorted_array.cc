d1b3ab3163d4ed3ae339d90a8d7b9d9191b971ac
Daniel Viktor <danhu849@student.liu.se>
2020-11-18 14:39:19 +0100

    initial commit lab5

---
+++ lab5/sorted_array.cc
@@ -0,0 +1,94 @@
+
+
+#include <utility>
+#include <stdexcept>
+
+using namespace std;
+
+template <typename T>
+sorted_array<T>::sorted_array(initializer_list<T> list)
+    : data{}
+{
+    for (T val : list)
+    {
+        insert(val);
+    }
+}
+
+template <typename T>
+void sorted_array<T>::insert(T value)
+{
+    data.push_back(value);
+    for (int i {size() - 1}; i > 0; --i)
+    {
+        if (data[i] < data[i-1])
+        {
+            swap(data[i-1], data[i]);
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+template <typename T>
+T sorted_array<T>::erase(int index)
+{
+    if (index < 0 || index >= size())
+        throw std::out_of_range{"unable to erase; index is out of bounds."};
+    for (int i {index}; i + 1 < size(); ++i)
+    {
+        swap(data[i], data[i+1]);
+    }
+    T value {data.back()};
+    data.pop_back();
+    return value;
+}
+
+template <typename T>
+T & sorted_array<T>::operator[](int index)
+{
+    return data[index];
+}
+
+template <typename T>
+T sorted_array<T>::operator[](int index) const
+{
+    return data[index];
+}
+
+template <typename T>
+T & sorted_array<T>::at(int index)
+{
+    return data.at(index);
+}
+
+template <typename T>
+T sorted_array<T>::at(int index) const
+{
+    return data.at(index);
+}
+
+template <typename T>
+int sorted_array<T>::size() const
+{
+    return static_cast<int>(data.size());
+}
+
+template <typename T>
+ostream & operator<<(ostream & os, sorted_array<T> const & array)
+{
+    if (array.size() == 0)
+    {
+        os << "{}";
+        return os;
+    }
+    os << '{' << array[0];
+    for (int i {1}; i < array.size(); ++i)
+    {
+        os << ", " << array[i];
+    }
+    os << '}';
+    return os;
+}
---
+++ lab5/sorted_array.h
@@ -0,0 +1,39 @@
+#ifndef SORTED_ARRAY_H
+#define SORTED_ARRAY_H
+
+#include <vector>
+#include <initializer_list>
+#include <ostream>
+
+template <typename T>
+class sorted_array
+{
+
+public:
+
+    sorted_array() = default;
+    sorted_array(std::initializer_list<T> list);
+
+    void insert(T value);
+    T erase(int index);
+
+    T & operator[](int index);
+    T operator[](int index) const;
+
+    T & at(int index);
+    T at(int index) const;
+
+    int size() const;
+
+private:
+
+    std::vector<T> data{};
+
+};
+
+template <typename T>
+std::ostream & operator<<(std::ostream & os, sorted_array<T> const & array);
+
+#include "sorted_array.cc"
+
+#endif//SORTED_ARRAY_H
