667e3b9a57f9020eeb818b998546c34110acb3c9
viktor <vikro653@student.liu.se>
2020-11-20 14:47:34 +0100

    added tests for string

--- lab5/sorted_array_test.cc
+++ lab5/sorted_array_test.cc
@@ -27,6 +27,17 @@ TEST_CASE("default initialization double")
   CHECK_THROWS (a.erase(5));
 }
 
+TEST_CASE("default initialization string")
+{
+  sorted_array<std::string> a;
+  CHECK (a.size() == 0);
+  CHECK_THROWS (a.at(0));
+  CHECK_THROWS (a.erase(0));
+  CHECK_THROWS (a.erase(-5));
+  CHECK_THROWS (a.erase(5));
+}
+
+
 TEST_CASE("insertion int, check comparator")
 {
   sorted_array<int> a;
@@ -53,6 +64,20 @@ TEST_CASE("insertion double")
   CHECK (a.size() == 2);
   CHECK (a[0] == 1);
   CHECK (a[1] == 5);
+}
+
+TEST_CASE("insertion string")
+{
+  sorted_array<std::string> a;
+  a.insert("b");
+  CHECK (a.size() == 1);
+  CHECK (a.at(0) == "b");
+  CHECK (a[0] == "b");
+
+  a.insert("a");
+  CHECK (a.size() == 2);
+  CHECK (a[0] == "a");
+  CHECK (a[1] == "b");
 }
 
 TEST_CASE("initialization int")
@@ -127,6 +152,44 @@ TEST_CASE("initialization double")
   CHECK (a[1] == 1);
   CHECK (a[2] == 5);
   CHECK (a[3] == 9);
+}
+
+TEST_CASE("initialization string")
+{
+  sorted_array<std::string> a{"fg", "cd", "cb", "a", "b"};
+  CHECK (a.size() == 5);
+  CHECK (a[0] == "a");
+  CHECK (a[1] == "b");
+  CHECK (a[2] == "cb");
+  CHECK (a[3] == "cd");
+  CHECK (a[4] == "fg");
+
+  a.insert("ax");
+  CHECK (a.size() == 6);
+  CHECK (a[0] == "a");
+  CHECK (a[1] == "ax");
+  CHECK (a[2] == "b");
+  CHECK (a[3] == "cb");
+  CHECK (a[4] == "cd");
+  CHECK (a[5] == "fg");
+
+
+  a.erase(5);
+  CHECK (a.size() == 5);
+  CHECK (a[0] == "a");
+  CHECK (a[1] == "ax");
+  CHECK (a[2] == "b");
+  CHECK (a[3] == "cb");
+  CHECK (a[4] == "cd");
+
+  CHECK_THROWS(a.erase(5));
+
+  a.erase(2);
+  CHECK (a.size() == 4);
+  CHECK (a[0] == "a");
+  CHECK (a[1] == "ax");
+  CHECK (a[2] == "cb");
+  CHECK (a[3] == "cd");
 }
 
 TEST_CASE("const array int")
@@ -222,6 +285,25 @@ TEST_CASE("const array double, testing distance")
   CHECK_THROWS (a.at(5));
 }
 
+TEST_CASE("const array string")
+{
+  sorted_array<string> const a{"a", "b", "c", "d", "e"};
+
+  CHECK (a.size() == 5);
+  CHECK (a[0] == "a");
+  CHECK (a[0] == a.at(0));
+  CHECK (a[1] == "b");
+  CHECK (a[1] == a.at(1));
+  CHECK (a[2] == "c");
+  CHECK (a[2] == a.at(2));
+  CHECK (a[3] == "d");
+  CHECK (a[3] == a.at(3));
+  CHECK (a[4] == "e");
+  CHECK (a[4] == a.at(4));
+  CHECK_THROWS (a.at(5));
+}
+
+
 TEST_CASE("print empty int")
 {
   std::ostringstream oss;
@@ -234,6 +316,14 @@ TEST_CASE("print empty double")
 {
   std::ostringstream oss;
   sorted_array<double> empty;
+  oss << empty;
+  CHECK (oss.str() == "{}");
+}
+
+TEST_CASE("print empty string")
+{
+  std::ostringstream oss;
+  sorted_array<std::string> empty;
   oss << empty;
   CHECK (oss.str() == "{}");
 }
@@ -252,4 +342,12 @@ TEST_CASE("print double")
   sorted_array<double> array{2.1, 3.1, 5.1, 7.1, 11.1, 13.1};
   oss << array;
   CHECK (oss.str() == "{2.1, 3.1, 5.1, 7.1, 11.1, 13.1}");
+}
+
+TEST_CASE("print string")
+{
+  std::ostringstream oss;
+  sorted_array<std::string> array{"xa", "bb", "hello", "emacs"};
+  oss << array;
+  CHECK (oss.str() == "{bb, emacs, hello, xa}");
 }
