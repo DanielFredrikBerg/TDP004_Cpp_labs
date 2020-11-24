6729c23414769b242577d3b41494caef14b224b2
danhu849 <danhu849@gitlab.liu.se>
2020-11-24 12:03:53 +0100

    have istream_iterator now, adds read strings to vector

--- lab6/main.cc
+++ lab6/main.cc
@@ -3,7 +3,10 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <regex>
+#include <vector>
+#include <iterator>
+
+
 
 //#include "word_list.h"
 
@@ -14,33 +17,45 @@ int main(int argc, char* argv[])
   // är ett s. Ifall s kolla om char innan är '. Om det är
   // ignorera och fortsätt leta efter nästa char bakifrån.
   // Ta substring mellan dessa tecken och lägg in som potentiellt ord.
-  string start_trash_str{"\"('"};
-  string end_trash_str{"!?);,.:\"'"};
-  bool char_start = false;
+   //std::string start_trash_str{"\"('"};
+   //std::string end_trash_str{"!?);,.:\"'"};
+   //bool char_start = false;
 
   if (argc > 1)
   {
+     std::vector<std::string> args{argv, argv + argc};
+     for (std::string word : args)
+     {
+        std::cout << word << std::endl;
+     }
+     
+// Ta reda p� exakt vad denna g�r:
+     std::vector<std::string> vs {istream_iterator<std::string> {ifs}, istream_iterator<std::string>{} }
+     
     std::string filename = argv[1];
     if(std::ifstream ifs{filename, std::ios::binary | std::ios::ate})
     {
-      std::stringstream str_stream;
+       
+    }
+    //   std::stringstream str_stream;
 
-      auto size = ifs.tellg();
-      std::cout << "result of size: " << size << std::endl;
-      std::string str(size, '\0');
-      ifs.seekg(0);
-      if( ifs.read(&str[0], size))
-      {
-        str_stream << str;
-        std::cout << str << std::endl;
-      }
+    //   auto size = ifs.tellg();
+    //   std::string str(size, '\0');
+    //   std::vector<std::string> s{};
+    //   ifs.seekg(0);
+    //   if (ifs.read(&str[0], size))
+    //   {
+    //      str_stream << str;
+    //      std::copy(std::begin(str_stream), std::end(str_stream), std::back_inserter(s));
+    //   }
 
       
-    }
-    else 
-    {
-      std::cout << "Too few arguments, please add .txt file to check."
-                << std::endl;
-    }  
+    //   std::cout << "result of size: " << size << std::endl;
+    // }
+    // else 
+    // {
+    //   std::cout << "Too few arguments, please add .txt file to check."
+    //             << std::endl;
+    // }  
   }
 }
