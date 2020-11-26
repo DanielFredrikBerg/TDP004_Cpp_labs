580e66fae85d143e3eca80d69815db992b38e1fd
danhu849 <danhu849@gitlab.liu.se>
2020-11-26 14:18:56 +0100

    ej giltig då den tar bort tecken som gör ordet ogiltigt i början

--- lab6/main.cc
+++ lab6/main.cc
@@ -10,35 +10,45 @@
 #include <locale>
 #include <set>
 
-std::string extract_alpha_subset(std::string word)
+std::string extract_alpha_subset(std::string& word)
 {
    std::locale swe_loc("sv_SE.UTF-8");
-   auto found = std::find_if( word.begin(), word.end(), [swe_loc](char c) { return std::isalpha(c, swe_loc); } );
+   auto found{std::find_if( word.begin(), word.end(), [&swe_loc](char c) { return std::isalpha(c, swe_loc); } )};
    if (found != word.end())
    {
       long int first_index{found - word.begin()};
-      auto found_rev = std::find_if( word.rbegin(), word.rend(), [swe_loc](char c) { return std::isalpha(c, swe_loc); } );
-      if (found_rev != word.rend())
+      auto last_index{word.find_last_not_of( "!?;,:.\"')" )};
+      if (word.at(last_index - 1) == '\'')
       {
-         long unsigned int second_index{found_rev - word.rbegin() + word.size()};
-         std::cout << "second find: " << *found_rev << std::endl;
-         return "substring( " + std::to_string(first_index) + ", " + std::to_string(second_index) + " );     "
-            + word.substr( first_index, second_index ); 
-         //return word.substr( first_index, second_index );
+         last_index = last_index - 2;
       }
+      return word.substr(first_index, last_index - first_index + 1);  
    }
    return "";
 }
 
+bool validate(std::string& s)
+{ 
+   if (s.size() < 3)
+   {
+      return false;
+   }
+   if(s.find("--") != std::string::npos)
+   {
+      return false;
+   }
+   if( s.find_last_of( "!?;,:.\"')" ) != std::string::npos )
+   {
+      return false;
+   }
+   return true;
+}
 
-//#include "word_list.h"
 
 int main(int argc, char* argv[])
 {
    if (argc > 1)
    {
-           
-// Ta reda på exakt vad denna gör:
       
       std::string filename = argv[1];
       if(std::ifstream ifs{filename, std::ios::in})
@@ -49,21 +59,30 @@ int main(int argc, char* argv[])
          
          //ta bort skräp fram och bak
          std::vector<std::string> less_trash; 
+         std::vector<std::string> less_t; 
+         
+         //std::cout << extract_alpha_subset(s) << std::endl; 
+         
+         std::transform(words.begin(), words.end(), std::back_inserter(less_trash),
+                        [](std::string s) -> auto {return extract_alpha_subset(s);});
 
-         //s.erase(std::remove_if(s.begin(), find(), [swe_loc](char c) {return !std::isalpha(c, swe_loc); } ), s.end() );
-            
-         for (std::string s : words)
-         {
-            std::cout << extract_alpha_subset(s) << std::endl; 
-         }
+         std::transform(less_trash.begin(), less_trash.end(), std::back_inserter(less_t),
+                        [](std::string s) -> std::string { if(validate(s)) {return s; } else {return "";}} );
          
          
-
-         std::copy(less_trash.begin(), less_trash.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
-         //std::transform(words.begin(), words.end(), std::back_inserter(less_trash),
-         //                [swe_loc](std::string s) -> auto {return });
+         std::copy(less_t.begin(), less_t.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+         
+      }
+      else
+      {
+         std::cout << "Error: Second argument missing or invalid.\nUsage: ./a.out FILE [-a] [-f] [-o N]" << std::endl;
       }
       
+   }
+   else
+   {
+      std::cout << "Error: No arguments given.\nUsage: a.out FILE [-a] [-f] [-o N]" << std::endl;
+      return -1;
    }
    return 0;
 }
