5d236a00f87a81a376e7e7b447522aa5ff19679c
danhu849 <danhu849@gitlab.liu.se>
2020-11-27 07:30:42 +0100

    made main_v main

--- lab6/main.cc
+++ lab6/main.cc
@@ -1,88 +1,209 @@
-#include <iostream>
+#include <iostream> 
+#include <exception>
+#include <algorithm> 
+#include <iterator>
 #include <iomanip>
 #include <fstream>
-#include <ostream>
-#include <sstream>
-#include <string>
+#include <utility> 
 #include <vector>
-#include <iterator>
-#include <algorithm>
+#include <cctype> 
 #include <locale>
-#include <set>
+#include <map>
 
-std::string extract_alpha_subset(std::string& word)
+bool is_valid(std::string const& str)
 {
-   std::locale swe_loc("sv_SE.UTF-8");
-   auto found{std::find_if( word.begin(), word.end(), [&swe_loc](char c) { return std::isalpha(c, swe_loc); } )};
-   if (found != word.end())
+   // words shorter than 3 characters are not valid
+   if (str.length() < 3)
    {
-      long int first_index{found - word.begin()};
-      auto last_index{word.find_last_not_of( "!?;,:.\"')" )};
-      if (word.at(last_index - 1) == '\'')
-      {
-         last_index = last_index - 2;
-      }
-      return word.substr(first_index, last_index - first_index + 1);  
+      return false;
    }
-   return "";
+
+   // words starting/ending with a '-' are not valid
+   if (str[0] == '-' || str[str.length() - 1] == '-')
+   {
+      return false;
+   }
+
+   // words with two '-' in a row are not valid
+   if (str.find("--") != std::string::npos)
+   {
+      return false;
+   }
+
+   // words that contain other characters than '-' or letters are not valid
+   std::locale swe_loc("sv_SE.UTF-8");
+   return std::all_of(str.begin(), str.end(), 
+                      [&swe_loc](char c)
+                      {return isalpha(c, swe_loc) || c == '-';});
 }
 
-bool validate(std::string& s)
-{ 
-   if (s.size() < 3)
+std::string clean_word(std::string & str)
+{
+   // find index of first/last letter in the word
+   auto first{str.find_first_not_of("\"'(")};
+   auto last{str.find_last_not_of("!?;,:.\"')")};
+
+   // adjust index for last letter in case it ends with 's
+   if (str[last - 1] == '\'')
    {
-      return false;
+      last -= 2;
    }
-   if(s.find("--") != std::string::npos)
-   {
-      return false;
-   }
-   if( s.find_last_of( "!?;,:.\"')" ) != std::string::npos )
-   {
-      return false;
-   }
-   return true;
+   return str.substr(first, last - first + 1);
+}
+
+
+void print_error(std::string const& error_msg)
+{
+   std::cerr << error_msg << std::endl;
+   std::cerr << "Usage: ./a.out FILE [-a] [-f] [-o N]" << std::endl;
 }
 
 
 int main(int argc, char* argv[])
 {
-   if (argc > 1)
+   if (argc < 2)
    {
-      
-      std::string filename = argv[1];
-      if(std::ifstream ifs{filename, std::ios::in})
-      {
-         std::vector<std::string> words{ std::istream_iterator<std::string>{ifs}, std::istream_iterator<std::string>{} };
-         std::copy(words.begin(), words.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
-
-         
-         //ta bort skräp fram och bak
-         std::vector<std::string> less_trash; 
-         std::vector<std::string> less_t; 
-         
-         //std::cout << extract_alpha_subset(s) << std::endl; 
-         
-         std::transform(words.begin(), words.end(), std::back_inserter(less_trash),
-                        [](std::string s) -> auto {return extract_alpha_subset(s);});
-
-         std::transform(less_trash.begin(), less_trash.end(), std::back_inserter(less_t),
-                        [](std::string s) -> std::string { if(validate(s)) {return s; } else {return "";}} );
-         
-         
-         std::copy(less_t.begin(), less_t.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
-         
-      }
-      else
-      {
-         std::cout << "Error: Second argument missing or invalid.\nUsage: ./a.out FILE [-a] [-f] [-o N]" << std::endl;
-      }
-      
+      print_error("Error: No arguments given."); 
+      return 1;
    }
-   else
+   else if (argc < 3 || argv[2][0] != '-' || 
+            (argv[2][1] != 'a' && argv[2][1] != 'f' && argv[2][1] != 'o'))
    {
-      std::cout << "Error: No arguments given.\nUsage: a.out FILE [-a] [-f] [-o N]" << std::endl;
-      return -1;
+      print_error("Error: Second argument missing or invalid.");
+      return 2;
    }
-   return 0;
+   else if (argv[2][1] == 'o')
+   {
+      try
+      {
+         std::stoi(argv[3]);
+      }
+      catch (std::exception &)
+      {
+         print_error("Error: Third argument missing or invalid.");
+         return 3;
+      }
+   }
+
+   // open file
+   std::ifstream in_stream;
+   in_stream.open(argv[1]);
+   if (!in_stream.is_open())
+   {
+      print_error("Error: Invalid file name.");
+      return 4;
+   }
+
+   // read potential words from file
+   std::vector<std::string> words;
+   std::copy(std::istream_iterator<std::string>{in_stream}, 
+             std::istream_iterator<std::string>{}, std::back_inserter(words));
+
+   // clean potential words
+   std::transform(words.begin(), words.end(), words.begin(), 
+                  [](std::string & str) { return clean_word(str); });
+
+   // validate clean words
+   auto it = std::remove_if(words.begin(), words.end(), 
+                            [](std::string const& str) {return !is_valid(str);});
+   words.erase(it, words.end());
+
+   // convert valid words to lowercase
+   std::transform(words.begin(), words.end(), words.begin(), 
+                  [](std::string & str) 
+                  { 
+                     std::transform(str.begin(), str.end(), str.begin(), 
+                                    [](char c){return std::tolower(c);});
+                     return str;
+                  });
+
+
+   // print valid words in alphabetical order with count
+   if (argv[2][1] == 'a')
+   {
+      // copy words into a map<string, int>
+      std::map<std::string, int> words_map;
+      transform(words.begin(), words.end(), inserter(words_map, words_map.begin()),
+                [&words] (std::string const& str)
+                {
+                   return make_pair(str, std::count(words.begin(), words.end(), str));
+                });
+
+      // clear vector
+      words.clear();
+
+      // copy words + count from map to vector
+      transform(words_map.begin(), words_map.end(), back_inserter(words), 
+                [&words_map] (std::pair<std::string, int> const& p) 
+                {
+                   return p.first + " " + std::to_string(p.second);
+                });
+
+      // print vector with words + count
+      std::copy(words.begin(), words.end(), 
+                std::ostream_iterator<std::string>(std::cout, "\n"));
+   } 
+   else if (argv[2][1] == 'f')
+   {
+      // create vector of unique words
+      std::sort(words.begin(), words.end());
+      std::vector<std::string> unique_words;
+      std::unique_copy(words.begin(), words.end(), std::back_inserter(unique_words));
+
+      // create vector of word count
+      std::vector<int> word_count;
+      std::transform(unique_words.begin(), unique_words.end(), 
+                     std::back_inserter(word_count), 
+                     [&words](std::string & str) 
+                     { return std::count(words.begin(), words.end(), str); });
+		
+      // merge into a vector of pairs
+      std::vector<std::pair<std::string, int>> pairs;
+      std::transform(unique_words.begin(), unique_words.end(), word_count.begin(), 
+                     std::back_inserter(pairs),
+                     [](std::string & str, int cnt) 
+                     { return std::make_pair(str, cnt); });
+
+
+      // sort vector<string, int> by int 
+      std::sort(pairs.begin(), pairs.end(), [](std::pair<std::string,int> l, \
+                                               std::pair<std::string,int> r) 
+                                            { return l.second > r.second; }); 
+
+      // print
+      std::transform(pairs.begin(), pairs.end(), 
+                     std::ostream_iterator<std::string>(std::cout, "\n" ), 
+                     [](std::pair<std::string, int> & p) 
+                     { return p.first + " " + std::to_string(p.second); } );
+   } 
+   else if (argv[2][1] == 'o')
+   {
+      std::stringstream str_stream;
+      unsigned long int length{0};
+      unsigned long int max_length{std::stoul(argv[3])};
+
+      auto copy_to_sstream = [&length, &max_length, &str_stream](std::string str) 
+                             {
+                                if (length != 0 
+                                    && length + str.length() + 1 >= max_length)
+                                {
+                                   str_stream << "\n";
+                                   length = 0;
+                                }
+                                else if (length > 0)
+                                {
+                                   str_stream << " ";
+                                   length++;
+                                }
+                                str_stream << str;
+                                length += str.length();
+                             };
+
+      std::for_each(words.begin(), words.end(), copy_to_sstream);
+
+      std::cout << str_stream.str() << std::endl; 
+
+   }
+ 
+
 }
